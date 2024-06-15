6af12d5edb79a972b15b2495e9acacb38fe143d5
tankh99 <khanghou99@gmail.com>
2024-06-15 10:51:26 +0800

    feat: add main.py

---
+++ README.md
@@ -0,0 +1,6 @@
+---
+title: bear-classifier
+app_file: chapter-2/prod.ipynb
+sdk: gradio
+sdk_version: 4.36.1
+---
---
+++ chapter-2/main.py
@@ -0,0 +1,17 @@
+import gradio as gr
+from fastai.vision.all import *
+
+learn = load_learner('export.pkl')
+
+categories = ("black", "grizzly", "teddy")
+
+def classify_image(img):
+  pred, idx, probs = learn.predict(img)
+  return dict(zip(categories, map(float, probs)))
+
+image = gr.Image()
+label = gr.Label()
+examples = ["examples/black.jpeg", "examples/grizzly.jpeg", "examples/teddy.jpg"]
+
+iface = gr.Interface(fn=classify_image, inputs=image, outputs=label, title="Bear Classifier")
+iface.launch(share=True)
